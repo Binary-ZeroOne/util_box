c3106a149b16afeb3ba072ee35c5ed5175ae0d4d
zero <none@none.com>
2018-08-20 20:56:14 +0800

    ServerResponse增加方法重载，修改ResponseCode为ResponseEnum

--- src/main/java/org/zero/utilitybox/common/ServerResponse.java
+++ src/main/java/org/zero/utilitybox/common/ServerResponse.java
@@ -3,7 +3,7 @@ package org.zero.utilitybox.common;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
-import org.zero.utilitybox.common.enums.ResponseCode;
+import org.zero.utilitybox.common.enums.ResponseEnum;
 
 import java.io.Serializable;
 
@@ -45,38 +45,58 @@ public class ServerResponse<T> implements Serializable {
 
     @JsonIgnore
     public boolean isSuccess() {
-        return this.code == ResponseCode.SUCCESS.getCode();
+        return this.code == ResponseEnum.SUCCESS.getCode();
     }
 
     public static <T> ServerResponse<T> createByCodeSuccess() {
-        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode());
+        return new ServerResponse<T>(ResponseEnum.SUCCESS.getCode());
     }
 
-    public static <T> ServerResponse<T> createBySuccess() {
-        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), ResponseCode.SUCCESS.getMsg());
+    public static <T> ServerResponse<T> createBySuccessMsg() {
+        return new ServerResponse<T>(ResponseEnum.SUCCESS.getCode(), ResponseEnum.SUCCESS.getMsg());
+    }
+
+    public static <T> ServerResponse<T> createBySuccessMsg(ResponseEnum successEnum) {
+        return new ServerResponse<T>(ResponseEnum.SUCCESS.getCode(), successEnum.getMsg());
     }
 
     public static <T> ServerResponse<T> createBySuccessMsg(String msg) {
-        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), msg);
+        return new ServerResponse<T>(ResponseEnum.SUCCESS.getCode(), msg);
     }
 
     public static <T> ServerResponse<T> createBySuccessData(T data) {
-        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), data);
+        return new ServerResponse<T>(ResponseEnum.SUCCESS.getCode(), data);
+    }
+
+    public static <T> ServerResponse<T> createBySuccessMsg(ResponseEnum successEnum, T data) {
+        return new ServerResponse<T>(ResponseEnum.SUCCESS.getCode(), successEnum.getMsg(), data);
     }
 
     public static <T> ServerResponse<T> createBySuccessMsg(String msg, T data) {
-        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), msg, data);
+        return new ServerResponse<T>(ResponseEnum.SUCCESS.getCode(), msg, data);
     }
 
-    public static <T> ServerResponse<T> createByError() {
-        return new ServerResponse<T>(ResponseCode.ERROR.getCode(), ResponseCode.ERROR.getMsg());
+    public static <T> ServerResponse<T> createByCodeError() {
+        return new ServerResponse<T>(ResponseEnum.ERROR.getCode());
     }
 
-    public static <T> ServerResponse<T> createByErrorMessage(String errorMsg) {
-        return new ServerResponse<T>(ResponseCode.ERROR.getCode(), errorMsg);
+    public static <T> ServerResponse<T> createByErrorMsg() {
+        return new ServerResponse<T>(ResponseEnum.ERROR.getCode(), ResponseEnum.ERROR.getMsg());
     }
 
-    public static <T> ServerResponse<T> createByErrorCodeMessage(int code, String errorMsg) {
+    public static <T> ServerResponse<T> createByErrorMsg(ResponseEnum errorEnum) {
+        return new ServerResponse<T>(ResponseEnum.ERROR.getCode(), errorEnum.getMsg());
+    }
+
+    public static <T> ServerResponse<T> createByErrorMsg(String errorMsg) {
+        return new ServerResponse<T>(ResponseEnum.ERROR.getCode(), errorMsg);
+    }
+
+    public static <T> ServerResponse<T> createByErrorCodeMsg(int code, String errorMsg) {
         return new ServerResponse<T>(code, errorMsg);
+    }
+
+    public static <T> ServerResponse<T> createByErrorCodeMsg(int code, ResponseEnum errorEnum) {
+        return new ServerResponse<T>(code, errorEnum.getMsg());
     }
 }
