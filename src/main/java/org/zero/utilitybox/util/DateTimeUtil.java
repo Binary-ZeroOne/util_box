642e5f76ba84a510fb70446c791d9ffb3ac95456
zeroJun <31101209+Binary-ZeroOne@users.noreply.github.com>
2019-03-01 14:44:35 +0800

    Update DateTimeUtil.java

--- src/main/java/org/zero/utilitybox/util/DateTimeUtil.java
+++ src/main/java/org/zero/utilitybox/util/DateTimeUtil.java
@@ -87,4 +87,148 @@ public class DateTimeUtil {
 
         return duration.getStandardDays();
     }
+    
+    /**
+     * 获取两个时间的相差天数
+     *
+     * @param beforeDate beforeDate
+     * @return long
+     */
+    public static long getDifferDays(Date beforeDate, Date lastDate) {
+        DateTime begin = new DateTime(beforeDate);
+        DateTime last = new DateTime(lastDate);
+        Duration duration = new Duration(begin, last);
+
+        return duration.getStandardDays();
+    }
+
+    /**
+     * 判断给定的日期是否早于当前日期
+     *
+     * @param date date
+     * @return boolean
+     */
+    public static boolean isBeforeNow(Date date) {
+        DateTime dateTime = new DateTime(date);
+
+        return dateTime.isBeforeNow();
+    }
+
+    /**
+     * 判断给定的日期是否早于当天日期
+     *
+     * @param date date
+     * @return boolean
+     */
+    public static boolean isBeforeToday(Date date) {
+        DateTime today = new DateTime().withTimeAtStartOfDay();
+        DateTime dateTime = new DateTime(date);
+
+        return dateTime.isBefore(today);
+    }
+
+    /**
+     * 判断给定的日期是否早于本周
+     *
+     * @param date date
+     * @return boolean
+     */
+    public static boolean isBeforeWeek(Date date) {
+        // 获取本周周一零点的时间
+        DateTime monday = new DateTime().dayOfWeek().setCopy(1).withTimeAtStartOfDay();
+        DateTime dateTime = new DateTime(date);
+
+        return dateTime.isBefore(monday);
+    }
+
+    /**
+     * 判断给定的日期是否早于本月
+     *
+     * @param date date
+     * @return boolean
+     */
+    public static boolean isBeforeMonth(Date date) {
+        // 获取本月一号
+        DateTime firsOfMonth = new DateTime().dayOfMonth().setCopy(1).withTimeAtStartOfDay();
+        DateTime dateTime = new DateTime(date);
+
+        return dateTime.isBefore(firsOfMonth);
+    }
+
+    /**
+     * 判断给定的日期是否是昨天
+     *
+     * @param date date
+     * @return boolean
+     */
+    public static boolean isYesterday(Date date) {
+        DateTime yesterday = new DateTime().minusDays(1).withTimeAtStartOfDay();
+        DateTime dateTime = new DateTime(date).withTimeAtStartOfDay();
+
+        return yesterday.isEqual(dateTime);
+    }
+
+    /**
+     * 判断给定的日期是否是上一周的
+     *
+     * @param date date
+     * @return boolean
+     */
+    public static boolean isLastWeek(Date date) {
+        // 获取上周一
+        DateTime monday = new DateTime().minusWeeks(1).dayOfWeek().setCopy(1).withTimeAtStartOfDay();
+        // 获取上周日
+        DateTime sunday = new DateTime().minusWeeks(1).dayOfWeek().setCopy(7)
+                .withTime(TWENTY_THREE, FIFTY_NINE, FIFTY_NINE, 999);
+        DateTime datetime = new DateTime(date);
+
+        return isInRange(datetime, monday, sunday);
+    }
+
+    /**
+     * 判断给定的日期是否是上个月的
+     *
+     * @param date date
+     * @return boolean
+     */
+    public static boolean isLastMonth(Date date) {
+        // 获取上月一号
+        DateTime firsOfMonth = new DateTime().minusMonths(1).dayOfMonth().setCopy(1)
+                .withTimeAtStartOfDay();
+        // 获取上月末
+        DateTime lastOfMonth = new DateTime().minusMonths(1).dayOfMonth()
+                .withMaximumValue().withTime(TWENTY_THREE, FIFTY_NINE, FIFTY_NINE, 999);
+        DateTime datetime = new DateTime(date);
+
+        return isInRange(datetime, firsOfMonth, lastOfMonth);
+    }
+
+    /**
+     * 减去时间，只保留日期
+     *
+     * @param dateTime dateTime
+     * @return DateTime
+     */
+    public static DateTime subTime(DateTime dateTime) {
+        String string = dateTime.toString(STANDARO_FORMAT);
+        string = string.substring(0, string.indexOf(" "));
+
+        return strToDateTime(string, DATE_FORMAT);
+    }
+
+    /**
+     * 判断一个日期是否在一个给定的时间范围内
+     *
+     * @param dateTime dateTime
+     * @param start    start
+     * @param end      end
+     * @return boolean
+     */
+    public static boolean isInRange(DateTime dateTime, DateTime start, DateTime end) {
+        if (dateTime.isEqual(start) || dateTime.isEqual(end)) {
+            return true;
+        }
+
+        return dateTime.isAfter(start) && dateTime.isBefore(end);
+    }
 }
